44687c776d8b62d5f38772d1593ae88067dc15fb
Dan Seminara <japaneseforgaming@gmail.com>
2021-08-14 20:13:18 -0400

    commit tree 342d22185bcb575decae72146aec0a102540f8c9

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+let totalBatteries = 0;
 // Code your solution here
+
+function reducer() {
+    
+    function batteries(totalBatterie, i) { return totalBatterie += i; }
+
+
+    totalBatteries =  batteryBatches.reduce(batteries, 0);
+}
+
+console.log(reducer());
